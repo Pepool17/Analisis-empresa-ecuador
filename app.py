d677673b548f84ca5e4ebca539812c9e002d069a
Paul Peñaherrera <paul.penaherrera@epn.edu.ec>
2024-06-25 19:03:16 -0500

    time series of total comments

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from shinywidgets import output_widget, render_widget
 from pathlib import Path
 import pandas as pd
 from src.functions import serie_tiempo_empresa, mapa_floresta
+
 app_ui = ui.page_fluid(
     ui.include_css(
         Path(__file__).parent / "styles.css"  
@@ -14,10 +15,13 @@ app_ui = ui.page_fluid(
     ui.div(
         ui.layout_column_wrap(
             1/2,  
-            ui.input_selectize('categoria', 'CATEGORIAS', ['Hoteles', 'Restaurantes', 'Bares']),
-            ui.input_selectize('nombre_empresa', 'NOMBRE', []),
+            ui.input_selectize('categoria', 'CATEGORIAS', ['Total', 'Hoteles', 'Restaurantes', 'Bares'], width = '50%'),
+            ui.div(
+                ui.output_ui("nombre_empresa_ui", inline=True),
+                style="width: 100%;"
+            )
         ),
-        ui.input_checkbox("toggle", "Mostrar Total", value=False),
+        ui.output_ui("toggle_checkbox"),
         ui.layout_column_wrap(
             1 / 2,
             ui.card(
@@ -27,7 +31,6 @@ app_ui = ui.page_fluid(
                 output_widget("plot_series_tiempo")
             )
         ),
-
     ).add_class("main-container"),
     
     ui.div(
@@ -46,53 +49,71 @@ app_ui = ui.page_fluid(
 
 # Server 
 def server(input, output, session):
+
+    @reactive.Calc
+    def get_dataframe():
+        if input.categoria() == 'Total':
+            return pd.read_csv('data/comentarios.csv')
+        elif input.categoria() == 'Hoteles':
+            return pd.read_csv('data/comentarios_hoteles.csv')
+        elif input.categoria() == 'Restaurantes':
+            return pd.read_csv('data/comentarios_restaurante.csv')
+        elif input.categoria() == 'Bares':
+            return pd.read_csv('data/comentarios_bares.csv')
+
     @reactive.Effect
-    def _():
-        if input.categoria():
-            if input.categoria() == 'Hoteles':
-                df = pd.read_csv('data/comentarios_hoteles.csv')
-            elif input.categoria() == 'Restaurantes':
-                df = pd.read_csv('data/comentarios_restaurante.csv')
-            elif input.categoria() == 'Bares':
-                df = pd.read_csv('data/comentarios_bares.csv')
+    @reactive.event(input.categoria)
+    def update_empresa_choices():
+        if input.categoria() != 'Total':
+            df = get_dataframe()
             ui.update_selectize('nombre_empresa', choices=list(df['Nombre'].unique()))
 
     @output
     @render.ui
+    def nombre_empresa_ui():
+        if input.categoria() != 'Total':
+            return ui.input_selectize('nombre_empresa', 'NOMBRE', [], width="50%")
+        else:
+            return ui.div()
+
+    @output
+    @render.ui
     def mapa_total():
-        if input.categoria() == 'Hoteles':
-            df = pd.read_csv('data/comentarios_hoteles.csv')
-        elif input.categoria() == 'Restaurantes':
-            df = pd.read_csv('data/comentarios_restaurante.csv')
-        elif input.categoria() == 'Bares':
-            df = pd.read_csv('data/comentarios_bares.csv')
-  
-        return mapa_floresta(df, nombre=input.nombre_empresa(), total = input.toggle())
+        df = get_dataframe()
+        nombre = input.nombre_empresa() if input.categoria() != 'Total' else None
+        mostrar_total = input.toggle() if input.categoria() != 'Total' else True
+        return mapa_floresta(df, nombre=nombre, total=mostrar_total)
+
+    @reactive.Calc
+    def get_time_series():
+        df = get_dataframe()
+        nombre = input.nombre_empresa() if input.categoria() != 'Total' else 'Total'
+        return serie_tiempo_empresa(df, nombre)
 
     @output
     @render_widget
     def plot_series_tiempo():
-        if input.categoria():
-            if input.categoria() == 'Hoteles':
-                df = pd.read_csv('data/comentarios_hoteles.csv')
-            elif input.categoria() == 'Restaurantes':
-                df = pd.read_csv('data/comentarios_restaurante.csv')
-            elif input.categoria() == 'Bares':
-                df = pd.read_csv('data/comentarios_bares.csv')
-                
-            series = serie_tiempo_empresa(df, input.nombre_empresa())
-            fig = go.Figure()
+        series = get_time_series()
+        fig = go.Figure()
 
-            for column in series.columns:
-                fig.add_trace(go.Scatter(x=series.index, y=series[column], mode='lines', name=column))
+        for column in series.columns:
+            fig.add_trace(go.Scatter(x=series.index, y=series[column], mode='lines', name=column))
 
-            fig.update_layout(
-                title=f"Serie de tiempo de comentarios para {input.nombre_empresa()}",
-                xaxis_title='Fecha',
-                yaxis_title='Número de comentarios',
-                template='plotly',
-                hovermode='x unified'
-            )
-            return fig
+        fig.update_layout(
+            title=f"Serie de tiempo de comentarios para {input.nombre_empresa() if input.categoria() != 'Total' else 'Total'}",
+            xaxis_title='Fecha',
+            yaxis_title='Número de comentarios',
+            template='plotly',
+            hovermode='x unified'
+        )
+        return fig
+    
+    @output
+    @render.ui
+    def toggle_checkbox():
+        if input.categoria() != 'Total':
+            return ui.input_checkbox("toggle", "Mostrar todos los locales.", value=True)
+        else:
+            return ui.div()
 
 app = App(app_ui, server)
--- src/functions.py
+++ src/functions.py
@@ -40,53 +40,16 @@ def dataframe_series_tiempo(df):
     df_conteo.columns = ['Malos', 'Neutros', 'Buenos']
     df_conteo = df_conteo.reset_index()
     df_conteo['Fecha'] = df_conteo['Año-Mes'].dt.to_timestamp()
-    df_conteo = df_conteo.drop('Año-Mes', axis=1)
-    return df_conteo
+    return df_conteo.drop('Año-Mes', axis=1).set_index('Fecha')
 
-
-def serie_tiempo_empresa(df, nombre_empresa):
+def serie_tiempo_empresa(df, nombre_empresa='Total'):
     df_conteo = dataframe_series_tiempo(df)
-    # Filtrar el DataFrame para la empresa específica
-    df_empresa = df_conteo[df_conteo['Nombre'] == nombre_empresa]
-    # Establecer la fecha como índice
-    df_empresa = df_empresa.set_index('Fecha')
-    # Seleccionar solo las columnas de interés
-    series = df_empresa[['Buenos', 'Neutros', 'Malos']]
-    return series
-
-
-def graficar_serie_tiempo(df, nombre_empresa):
-    series = serie_tiempo_empresa(df, nombre_empresa)
-    fig = go.Figure()
-
-    # Añadir las series de tiempo al gráfico
-    for column in series.columns:
-        fig.add_trace(go.Scatter(x=series.index, y=series[column], mode='lines', name=column))
-    
-    # Configurar el layout del gráfico
-    fig.update_layout(
-        title=f"Serie de tiempo de comentarios para {nombre_empresa}",
-        xaxis_title='Fecha',
-        yaxis_title='Número de comentarios',
-        template='plotly',
-        hovermode='x unified'
-    )
-
+    if nombre_empresa != 'Total':
+        return df_conteo[df_conteo['Nombre'] == nombre_empresa][['Buenos', 'Neutros', 'Malos']]
+    return df_conteo.groupby('Fecha')[['Buenos', 'Neutros', 'Malos']].sum()
 
 def convertir_coordenadas(arreglo):
-
-    coordenadas = []
-    for coord_str in arreglo:
-        # Eliminar paréntesis y comillas
-        coord_str = coord_str.replace("(", "").replace(")", "").replace("'", "")
-        # Separar la latitud y longitud
-        lat_str, lon_str = coord_str.split(", ")
-        # Convertir a floats
-        lat = float(lat_str)
-        lon = float(lon_str)
-        # Añadir a la lista de coordenadas
-        coordenadas.append((lat, lon))
-    return coordenadas
+    return [tuple(map(float, coord.strip("()").replace("'", "").split(", "))) for coord in arreglo]
 
 
 def calcular_punto_medio(coordenadas):
@@ -122,66 +85,34 @@ def calcular_punto_medio(coordenadas):
 
 def extraer_info_empresa(df, nombre):
     try:
-        df_pivot = df.pivot_table(index='Nombre', values=['Latitud', 'Longitud', 'Dirección', 'Tipo Establecimiento'], aggfunc='first')
-        df_empresa = df_pivot.loc[nombre, ['Latitud', 'Longitud', 'Dirección', 'Tipo Establecimiento']]
-        df_empresa = df_empresa.fillna('')
-        return [[df_empresa['Latitud'], df_empresa['Longitud']], df_empresa['Dirección'], df_empresa['Tipo Establecimiento']]
-    except KeyError:
-        #print(f'No se encontró la empresa con el nombre {nombre} en el DataFrame.')
+        empresa = df[df['Nombre'] == nombre].iloc[0]
+        return [[empresa['Latitud'], empresa['Longitud']], empresa['Dirección'], empresa['Tipo Establecimiento']]
+    except (IndexError, KeyError):
         return None
-    except Exception as e:
-        #print(f'Error inesperado: {e}')
-        return None
-
 
 def mapa_floresta(df, nombre, total):
     def generar_popup(tipo, nombre, coord, dir):
-        popup = f'<div style="text-align: center; width: 200px;">'
-        if tipo:
-            popup += f'<b>{tipo}</b><br>'
-        if nombre:
-            popup += f'<b>{nombre}</b><br>'
-        if coord:
-            popup += f'<b>({coord[0]}, {coord[1]})</b><br>'
-        if dir:
-            popup += f'<b>{dir}</b>'
-        popup += '</div>'
-        return popup
+        return f'<div style="text-align: center; width: 200px;"><b>{tipo}</b><br><b>{nombre}</b><br><b>({coord[0]}, {coord[1]})</b><br><b>{dir}</b></div>'
 
-    if not total:
+    if not total and nombre:
         info_empresa = extraer_info_empresa(df, nombre)
         if info_empresa is None:
-            #print(f"No se pudo obtener la información de la empresa: {nombre}")
-            return None
-
+            return folium.Map(location=[4.60971, -74.08175], zoom_start=13)  # Coordenadas de Bogotá
         coord, dir, tipo = info_empresa
-        popuptext = generar_popup(tipo, nombre, coord, dir)
-        floresta = folium.Map(location=coord, zoom_start=18)
-        folium.Marker(location=coord, popup=folium.Popup(popuptext, max_width=300), icon=folium.Icon(color='red')).add_to(floresta)
-        folium.Circle(location=coord, color='red', fill_color='red', radius=10, weight=4, fill_opacity=0.5).add_to(floresta)
-        return floresta
+        mapa = folium.Map(location=coord, zoom_start=18)
+        folium.Marker(location=coord, popup=folium.Popup(generar_popup(tipo, nombre, coord, dir), max_width=300), 
+                      icon=folium.Icon(color='red')).add_to(mapa)
+        return mapa
 
-    try:
-        coordenadas = convertir_coordenadas(df['Coordenadas'].unique())
-        punto_medio = calcular_punto_medio(coordenadas)
-        floresta = folium.Map(location=punto_medio, zoom_start=16)
+    coordenadas = convertir_coordenadas(df['Coordenadas'].unique())
+    punto_medio = calcular_punto_medio(coordenadas)
+    mapa = folium.Map(location=punto_medio, zoom_start=16)
 
-        for nombres in df['Nombre'].dropna().unique():
-            info_empresa = extraer_info_empresa(df, nombres)
-            if info_empresa is None:
-                #print(f"No se pudo obtener la información de la empresa: {nombres}")
-                continue
+    for _, row in df.drop_duplicates('Nombre').iterrows():
+        coord = [row['Latitud'], row['Longitud']]
+        popuptext = generar_popup(row['Tipo Establecimiento'], row['Nombre'], coord, row['Dirección'])
+        color = 'red' if row['Nombre'] == nombre else 'blue'
+        folium.Marker(location=coord, popup=folium.Popup(popuptext, max_width=300), 
+                      icon=folium.Icon(color=color)).add_to(mapa)
 
-            coord, dir, tipo = info_empresa
-            popuptext = generar_popup(tipo, nombres, coord, dir)
-
-            if nombres == nombre:
-                folium.Circle(location=coord, color='red', fill_color='red', radius=12, weight=5, fill_opacity=0.5).add_to(floresta)
-                folium.Marker(location=coord, popup=folium.Popup(popuptext, max_width=300), icon=folium.Icon(color='red')).add_to(floresta)
-            else:
-                folium.Circle(location=coord, color='blue', fill_color='blue', radius=10, weight=4, fill_opacity=0.5).add_to(floresta)
-                folium.Marker(location=coord, popup=folium.Popup(popuptext, max_width=300), icon=folium.Icon(color='blue')).add_to(floresta)
-        return floresta
-    except Exception as e:
-        #print(f"Error inesperado: {e}")
-        return None
+    return mapa
